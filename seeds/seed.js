ddb060fb7bd9f929940fb2168f98a536341d87f8
trippjoe <tripp.j@hotmail.com>
2021-10-27 11:45:21 -0400

    added user seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,13 @@
 const sequelize = require('../config/connection');
 const seedHoldings = require('./holding-seeds');
-
-// TODO: implement user seeds, implement transaction seeds
-// const seedUsers = require('./user-seeds');
+const seedUsers = require('./user-seeds');
 // const seedTransactions = require('./transaction-seeds');
 
 const seedAll = async () => {
     await sequelize.sync({force: true});
+    await seedUsers();
     await seedHoldings();
 
-    // await seedUsers();
     // await seedTransactions();
 }
 
