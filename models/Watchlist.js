f09b4287b0a91eba97a860759de123b28ed12c64
trippjoe <tripp.j@hotmail.com>
2021-10-27 19:39:54 -0400

    updated path to javascript file from handlebars login/signup

---
+++ models/Watchlist.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Watchlist extends Model{}
+
+Watchlist.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    symbol: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    }
+},
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: false,
+        modelName: 'watchlist'
+    }
+);
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
 app.use(routes);
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
