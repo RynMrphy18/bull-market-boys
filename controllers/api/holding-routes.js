5dba392bd49cf4d0adae5dfd6061162b787f750b
trippjoe <tripp.j@hotmail.com>
2021-10-28 01:40:47 -0400

    added holdings route file

---
+++ controllers/api/holding-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {User, Holding, Transaction} = require('../../models');
+
+router.get('/:symbol', (req, res) => {
+    Holding.findAll({
+        where: {
+            symbol: req.params.symbol
+        }
+    })
+    .then(holdings => {
+        if(holdings.length < 1){
+            return res.status(404).json({message: 'No holdings found with this symbol.'});
+        }
+        return res.render('single-stock', {
+            holdings: holdings.map(holding => holding.toJSON())
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
+const holdingRoutes = require('./holding-routes.js');
 const stockRoutes = require('./stock-routes.js');
 
 router.use('/users', userRoutes);
+router.use('/holdings', holdingRoutes);
 router.use('/stocks', stockRoutes);
 
 module.exports = router;
