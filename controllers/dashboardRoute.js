57a852326545732091930e39540e549a5f7623d7
trippjoe <tripp.j@hotmail.com>
2021-10-28 00:48:15 -0400

    login and log out working properly now

--- controllers/dashboardRoute.js
+++ controllers/dashboardRoute.js
@@ -3,7 +3,9 @@ const withAuth = require('../utils/auth.js');
 
 // display the users main screen (once logged in)
 router.get('/', withAuth, (req, res) => {
-    return res.render('dashboard');
+    return res.render('dashboard', {
+        loggedIn: req.session.loggedIn
+    });
 });
 
 module.exports = router;
