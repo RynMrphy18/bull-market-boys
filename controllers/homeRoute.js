1e1f5c53607014364da1bb23fe3ce9199acb4e9d
trippjoe <tripp.j@hotmail.com>
2021-10-28 20:00:52 -0400

    logged in on homepage doesnt show login and signup

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,10 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    if (req.session.loggedIn) {
-        return res.redirect('/dashboard');
-    }
-    return res.render('index');
+    return res.render('index', {loggedIn: req.session.loggedIn});
 });
 
 router.get('/login', (req, res) => {
