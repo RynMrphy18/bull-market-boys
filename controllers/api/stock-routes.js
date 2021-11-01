1366f2e12d95a8338bd618a80cbef793d0f8d3c5
trippjoe <tripp.j@hotmail.com>
2021-11-01 13:19:57 -0400

    displaying information about stocks on page after searching

--- controllers/api/stock-routes.js
+++ controllers/api/stock-routes.js
@@ -2,32 +2,40 @@ const router = require('express').Router();
 const yahooFinance = require('yahoo-finance');
 
 // query yahoo finance for stock data
-// update stock table with new data queried
-router.post('/', async (req, res) => {
-    let symbol = req.body.search;
+router.get('/:symbol', async (req, res) => {
 
-    let stockData = await yahooFinance.quote({
+    const symbol = req.params.symbol;
+
+    // let stockData = await yahooFinance.quote({
+    //     symbol: symbol,
+    //     modules: [ 'price', 'summaryDetail' ]
+    // });
+
+    yahooFinance.quote({
         symbol: symbol,
         modules: [ 'price', 'summaryDetail' ]
-    });
+    })
+    .then(response => {
+        // create an object with important info from the stock data
 
-    if(!stockData){
+        let stockData = response;
+
+        let stock = {
+            symbol: symbol,
+            price: stockData.price.regularMarketPrice,
+            open: stockData.summaryDetail.open,
+            // close: stockData.summaryDetail.close,
+            high: stockData.summaryDetail.dayHigh,
+            low: stockData.summaryDetail.dayLow,
+            yearHigh: stockData.summaryDetail.fiftyTwoWeekHigh,
+            yearLow: stockData.summaryDetail.fiftyTwoWeekLow,
+        }
+
+        return res.render('single-stock', {stock});
+    })
+    .catch(err => {
         return res.status(400).json({ message: 'No stock with that symbol!' });
-    }
-
-    // create an object with important info from the stock data
-    let stock = {
-        symbol: symbol,
-        price: stockData.price.regularMarketPrice,
-        open: stockData.summaryDetail.open,
-        close: stockData.summaryDetail.close,
-        high: stockData.summaryDetail.dayHigh,
-        low: stockData.summaryDetail.dayLow,
-        yearHigh: stockData.summaryDetail.fiftyTwoWeekHigh,
-        yearLow: stockData.summaryDetail.fiftyTwoWeekLow,
-    }
-
-    return res.json(stock);
+    });
 });
 
 module.exports = router;
--- public/javascript/search.js
+++ public/javascript/search.js
@@ -6,20 +6,7 @@ async function searchFormHandler(event) {
     const search = document.querySelector('#search-field').value.trim();
 
     if(search) {
-        const response = await fetch('/api/stocks', {
-            method: 'post',
-            body: JSON.stringify({
-                search
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-
-        if(response.ok){
-            // take the user to the individual stock page for the stock they searched
-            // document.location.replace('/dashboard/');
-        }else{
-            alert(response.statusText);
-        }
+        document.location.replace(`/api/stocks/${search}`);
     }
 }
 
