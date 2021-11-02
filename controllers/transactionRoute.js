f31526fda5d51805f2c97c17cc264dc24358d938
trippjoe <tripp.j@hotmail.com>
2021-11-02 13:31:40 -0400

    color coding idea for transaction display page

--- controllers/transactionRoute.js
+++ controllers/transactionRoute.js
@@ -11,7 +11,7 @@ router.get('/:symbol', (req, res) => {
     Transaction.findAll({where: {symbol: req.params.symbol, user_id: req.session.user_id}})
     .then(transactions => {
         if(transactions.length > 0){
-            return res.render('transactions', {transactions});
+            return res.render('transactions', {transactions, loggedIn: req.session.loggedIn});
         }
         return res.status(404).json('No transactions found!');
     });
