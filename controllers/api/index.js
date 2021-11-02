45960d9205ae82e42c4c2f31fc66e73c407d18eb
trippjoe <tripp.j@hotmail.com>
2021-11-02 11:59:23 -0400

    added transactionRoute, when user clicks on a holding it will bring them to their transactions of that stock.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const holdingRoutes = require('./holding-routes.js');
-const transactionRoutes = require('./transaction-routes');
 
 router.use('/users', userRoutes);
 router.use('/holdings', holdingRoutes);
-router.use('/transaction', transactionRoutes);
 
 module.exports = router;
---
+++ controllers/transactionRoute.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {Transaction, Holding} = require('../models');
+
+router.get('/', (req, res) => {
+    Transaction.findAll({})
+    .then(transactions => res.json(transactions))
+    .catch(err => console.log(err));
+});
+
+router.get('/:symbol', (req, res) => {
+    Transaction.findAll({where: {symbol: req.params.symbol, user_id: req.session.user_id}})
+    .then(transactions => {
+        if(transactions.length > 0){
+            // res.json(transactions);
+            console.log(transactions);
+            return res.render('transactions', {transactions});
+        }
+        return res.status(404).json('No transactions found!');
+    });
+})
+
+module.exports = router;
