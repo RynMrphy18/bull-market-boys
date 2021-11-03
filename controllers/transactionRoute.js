e4ff23f6b75ca9ce10b555e9ca9763230140d13f
JPDBrodeur <jbrodeur001@gmail.com>
2021-11-02 20:11:43 -0400

    passed stockData into transactions route

--- controllers/transactionRoute.js
+++ controllers/transactionRoute.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const {Transaction, Holding} = require('../models');
+const getStock = require('../utils/getStock')
 
 router.get('/', (req, res) => {
     Transaction.findAll({})
@@ -8,10 +9,12 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:symbol', (req, res) => {
-    Transaction.findAll({where: {symbol: req.params.symbol, user_id: req.session.user_id}})
+    const symbol = req.params.symbol;
+    Transaction.findAll({where: {symbol: symbol, user_id: req.session.user_id}})
     .then(transactions => {
         if(transactions.length > 0){
-            return res.render('transactions', {transactions, loggedIn: req.session.loggedIn});
+            let stockData = getStock(symbol);
+            return res.render('transactions', {transactions, stockData, loggedIn: req.session.loggedIn});
         }
         return res.status(404).json('No transactions found!');
     });
---
+++ utils/getStock.js
@@ -0,0 +1,24 @@
+const yahooFinance = require('yahoo-finance');
+
+const getStock = async (symbol) => {
+    return await yahooFinance.quote({
+    symbol: symbol,
+    modules: [ 'price', 'summaryDetail' ] // see the docs for the full list
+    })
+    .then((stockData) => {
+        let stockInfo = {
+            name: stockData.price.longName,
+            symbol: stockData.price.symbol,
+            price: stockData.price.regularMarketPrice,
+            open: stockData.summaryDetail.open,
+            high: stockData.summaryDetail.dayHigh,
+            low: stockData.summaryDetail.dayLow,
+            yearHigh: stockData.summaryDetail.fiftyTwoWeekHigh,
+            yearLow: stockData.summaryDetail.fiftyTwoWeekLow,
+        }
+        console.log(stockInfo)
+        return stockInfo;
+    })
+}
+
+module.exports = getStock;
