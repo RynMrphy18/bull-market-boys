098bed347c9e05e30c64c2435e94497bc65f2edb
trippjoe <tripp.j@hotmail.com>
2021-11-03 01:01:33 -0400

    changing database url for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.CLEARDB_COPPER_URL
+    // ? new Sequelize(process.env.JAWSDB_URL)
+    ? new Sequelize(process.env.CLEARDB_COPPER_URL)
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
