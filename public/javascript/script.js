3b42abfb902eaea5f1eb8fad2dae2d44db4b0d84
Matthew Pandolfo <matt@pandolfo.com>
2021-11-03 21:53:42 -0400

    fix classlist error on main page

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,40 +1,65 @@
 let darkMode = localStorage.getItem("darkMode");
 const darkModeBtn = document.querySelector(".darkModeBtn");
-const darkModeHeader = document.querySelector('header')
-const darkModeFooter = document.querySelector('footer')
-const darkModeSearch = document.querySelector('.symbolSearch')
+const darkModeHeader = document.querySelector("header");
+const darkModeFooter = document.querySelector("footer");
+const darkModeSearch = document.querySelector(".symbolSearch");
 
+let route = window.location.pathname;
 
 const enableDarkMode = () => {
-  document.body.classList.add("darkmode");
-  // toggle btn icon
-  darkModeBtn.classList.add('fa-moon')
-  darkModeBtn.classList.remove('fa-sun')
+  if (route === "/dashboard") {
+    document.body.classList.add("darkmode");
+    // toggle btn icon
+    darkModeBtn.classList.add("fa-moon");
+    darkModeBtn.classList.remove("fa-sun");
 
-  darkModeHeader.classList.add("darkmodeHeader");
-  darkModeFooter.classList.add("darkmodeFooter");
-  darkModeSearch.classList.add("darkmodeSearch");
-  localStorage.setItem("darkMode", "enabled");
+    darkModeHeader.classList.add("darkmodeHeader");
+    darkModeFooter.classList.add("darkmodeFooter");
+    darkModeSearch.classList.add("darkmodeSearch");
+    localStorage.setItem("darkMode", "enabled");
+  } else {
+    document.body.classList.add("darkmode");
+    // toggle btn icon
+    darkModeBtn.classList.add("fa-moon");
+    darkModeBtn.classList.remove("fa-sun");
+
+    darkModeHeader.classList.add("darkmodeHeader");
+    darkModeFooter.classList.add("darkmodeFooter");
+    localStorage.setItem("darkMode", "enabled");
+  }
 };
 
 const disableDarkMode = () => {
-  document.body.classList.remove("darkmode");
-  // toggle btn icon
-  darkModeBtn.classList.remove('fa-moon')
-  darkModeBtn.classList.add('fa-sun')
+  if (route === "/dashboard") {
+    document.body.classList.remove("darkmode");
+    // toggle btn icon
+    darkModeBtn.classList.remove("fa-moon");
+    darkModeBtn.classList.add("fa-sun");
 
-  darkModeHeader.classList.remove("darkmodeHeader");
-  darkModeFooter.classList.remove("darkmodeFooter");
-  darkModeSearch.classList.remove("darkmodeSearch");
-  localStorage.setItem("darkMode", null);
+    darkModeHeader.classList.remove("darkmodeHeader");
+    darkModeFooter.classList.remove("darkmodeFooter");
+    darkModeSearch.classList.remove("darkmodeSearch");
+    localStorage.setItem("darkMode", null);
+  } else {
+    document.body.classList.remove("darkmode");
+    // toggle btn icon
+    darkModeBtn.classList.remove("fa-moon");
+    darkModeBtn.classList.add("fa-sun");
+
+    darkModeHeader.classList.remove("darkmodeHeader");
+    darkModeFooter.classList.remove("darkmodeFooter");
+    localStorage.setItem("darkMode", null);
+  }
 };
 
-if (darkMode === 'enabled') {
-  enableDarkMode()
+if (darkMode === "enabled") {
+  enableDarkMode();
+} else {
+  disableDarkMode();
 }
 
 darkModeBtn.addEventListener("click", () => {
-  darkMode = localStorage.getItem('darkMode')
+  darkMode = localStorage.getItem("darkMode");
   if (darkMode !== "enabled") {
     enableDarkMode();
   } else {
