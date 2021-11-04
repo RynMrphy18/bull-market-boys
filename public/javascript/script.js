9ed5b5ccafce01f60b56bcfbd50c10a5828bb5e2
Matthew Pandolfo <matt@pandolfo.com>
2021-11-04 06:58:02 -0400

    login and logout input darkmode

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -2,46 +2,30 @@ let darkMode = localStorage.getItem("darkMode");
 const darkModeBtn = document.querySelector(".darkModeBtn");
 const darkModeHeader = document.querySelector("header");
 const darkModeFooter = document.querySelector("footer");
-const darkModeSearch = document.querySelector(".symbolSearch");
+const darkModeTable = document.querySelector(".table");
+const darkmodeInput = document.querySelectorAll(".form-control");
 
 let route = window.location.pathname;
 
 const enableDarkMode = () => {
-  if (route === "/dashboard") {
-    document.body.classList.add("darkmode");
-    // toggle btn icon
-    darkModeBtn.classList.add("fa-moon");
-    darkModeBtn.classList.remove("fa-sun");
+  document.body.classList.add("darkmode");
+  // toggle btn icon
+  darkModeBtn.classList.add("fa-moon");
+  darkModeBtn.classList.remove("fa-sun");
 
-    darkModeHeader.classList.add("darkmodeHeader");
-    darkModeFooter.classList.add("darkmodeFooter");
-    darkModeSearch.classList.add("darkmodeSearch");
-    localStorage.setItem("darkMode", "enabled");
-  } else {
-    document.body.classList.add("darkmode");
-    // toggle btn icon
-    darkModeBtn.classList.add("fa-moon");
-    darkModeBtn.classList.remove("fa-sun");
+  darkModeHeader.classList.add("darkmodeHeader");
+  darkModeFooter.classList.add("darkmodeFooter");
+  localStorage.setItem("darkMode", "enabled");
 
-    darkModeHeader.classList.add("darkmodeHeader");
-    darkModeFooter.classList.add("darkmodeFooter");
-    localStorage.setItem("darkMode", "enabled");
+  if (route === "/dashboard/" || route === "/login" || route === "/signup") {
+    for (let i = 0; i < darkmodeInput.length; i++) {
+      darkmodeInput[i].classList.add('darkmodeInput')
+    }
   }
 };
 
 const disableDarkMode = () => {
-  if (route === "/dashboard") {
-    document.body.classList.remove("darkmode");
-    // toggle btn icon
-    darkModeBtn.classList.remove("fa-moon");
-    darkModeBtn.classList.add("fa-sun");
-
-    darkModeHeader.classList.remove("darkmodeHeader");
-    darkModeFooter.classList.remove("darkmodeFooter");
-    darkModeSearch.classList.remove("darkmodeSearch");
-    localStorage.setItem("darkMode", null);
-  } else {
-    document.body.classList.remove("darkmode");
+  document.body.classList.remove("darkmode");
     // toggle btn icon
     darkModeBtn.classList.remove("fa-moon");
     darkModeBtn.classList.add("fa-sun");
@@ -49,7 +33,12 @@ const disableDarkMode = () => {
     darkModeHeader.classList.remove("darkmodeHeader");
     darkModeFooter.classList.remove("darkmodeFooter");
     localStorage.setItem("darkMode", null);
-  }
+
+    if (route === "/dashboard/" || route === "/login" || route === "/signup") {
+      for (let i = 0; i < darkmodeInput.length; i++) {
+        darkmodeInput[i].classList.remove('darkmodeInput')
+      }
+    }
 };
 
 if (darkMode === "enabled") {
