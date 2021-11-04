f0c1f6c0a3a23f20c9729abb511182637d7579fb
JPDBrodeur <jbrodeur001@gmail.com>
2021-11-03 22:25:17 -0400

    remove duplicate buttons on home page / spacing corrections

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    return res.render('index', {loggedIn: req.session.loggedIn});
+    return res.render('index', {loggedIn: req.session.loggedIn, home: true});
 });
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         return res.redirect('/dashboard');
     }
-    return res.render('login', {login: true});
+    return res.render('login', {login: true,});
 });
 
 router.get('/signup', (req, res) => {
